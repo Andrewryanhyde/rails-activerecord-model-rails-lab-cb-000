51bab0840d69534fd1b40afba0b80cc8ac1b67f8
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-19 13:36:16 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,9 @@
 class Student < ActiveRecord::Base
 
   def to_s
-    Student.all.each do |student| 
+    Student.all.each do |student|
       string = student.first_name + " " + student.last_name
-      string 
+      string
     end
   end
 
